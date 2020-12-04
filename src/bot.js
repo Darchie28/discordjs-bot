0ec9646e206d86e1b56425bc10811036f8439fd7
Denzel Archie <archie_denzel@yahoo.com>
2020-12-04 03:06:51 -0600

    stopped tracking env file

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,24 @@
 require('dotenv').config();
 
 console.log(process.env.DISCORDJS_BOT_TOKEN);
+
+const { Client } = require('discord.js');
+const client = new Client();
+const PREFIX = "$";
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in`)
+});
+
+client.on('message', (message) => {
+    // handle message from bot
+    if (message.author.bot) return;
+    if (message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = messge.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
+    }
+})
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
